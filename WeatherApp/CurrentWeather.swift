efbd337d327a90eba2668ce3482fc2c07bdd5bb6
Igor Dikan <igor.ivanovich.dikan@mail.ru>
2020-09-25 22:49:04 +0400

    Создание CurrentWeather.swift и WeatherIconManager.swift

---
+++ WeatherApp/CurrentWeather.swift
@@ -0,0 +1,26 @@
+//
+//  CurrentWeather.swift
+//  WeatherApp
+//
+//  Created by Игорь Дикань on 25.09.2020.
+//  Copyright © 2020 Игорь Дикань. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct CurrentWeather {
+//    @IBOutlet weak var lacationLabel: UILabel!
+//    @IBOutlet weak var imageView: UIImageView!
+//    @IBOutlet weak var pressureLabel: UILabel!
+//    @IBOutlet weak var humidityLabel: UILabel!
+//    @IBOutlet weak var temperatureLabel: UILabel!
+//    @IBOutlet weak var appearentTemperatureLabel: UILabel!
+//    @IBOutlet weak var refreshButton: UIButton!
+    
+    let temperature: Double
+    let appearentTemperature: Double
+    let humidity: Double
+    let pressure: Double
+    let icon: UIImage
+}
