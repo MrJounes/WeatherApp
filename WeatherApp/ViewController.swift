94f03b7a994d641618632bcbe75912e81e6d83d6
Igor Dikan <igor.ivanovich.dikan@mail.ru>
2020-09-25 19:55:22 +0400

    Первый коммит

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Игорь Дикань on 25.09.2020.
+//  Copyright © 2020 Игорь Дикань. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
