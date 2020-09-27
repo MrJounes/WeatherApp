a1aab54344487a24763a9c3bb9975a75229802f0
Igor Dikan <igor.ivanovich.dikan@mail.ru>
2020-09-27 21:38:10 +0400

    Реализация первого метода APIManager, добавлен @escaping в код проекта для устранения ошибки XCode

--- WeatherApp/APIManager.swift
+++ WeatherApp/APIManager.swift
@@ -25,3 +25,46 @@ protocol APIManager {
     
     init(sessionConfiguration: URLSessionConfiguration)
 }
+
+extension APIManager {
+    func JSONTaskWith(request: URLRequest, completionHandler: @escaping JSONCompletionHandler ) -> JSONTask {
+        let dataTask = session.dataTask(with: request) { (data, response, error) in
+            
+            guard let HTTPResponse = response as? HTTPURLResponse else {
+                
+                let userInfo = [
+                    NSLocalizedDescriptionKey: NSLocalizedString("Missing HTTP Response", comment: "")
+                ]
+                let error = NSError(domain: SWINetworkingErrorDomain, code: 100, userInfo: userInfo)
+                
+                completionHandler(nil, nil, error)
+                
+                return
+            }
+            
+            if data == nil {
+                if let error = error {
+                    completionHandler(nil, HTTPResponse, error)
+                }
+            } else {
+                switch HTTPResponse.statusCode {
+                case 200:
+                    do {
+                        let json = try  JSONSerialization.jsonObject(with: data!, options: []) as? [String : AnyObject]
+                        completionHandler(json, HTTPResponse, nil)
+                        
+                    } catch let error as NSError {
+                        completionHandler(nil, HTTPResponse, error)
+                    }
+                default:
+                    print("We hawe got a response status \(HTTPResponse.statusCode)")
+                }
+            }
+        }
+        return dataTask
+    }
+    
+    func fetch<T>(request: URLRequest, parse: ([String : AnyObject]?) -> T?, completionHandler: (APIResult<T>) -> Void) {
+        
+    }
+}
