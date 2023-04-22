4d599c30e19a4d648672e2482e9837c12dd8df64
Igor Dikan <idikan@yandex-team.ru>
2023-04-22 22:01:52 +0300

    Реализован сервис работы с геопозицией

---
+++ WeatherApp/Core/ServiceLayer/LocationManager.swift
@@ -0,0 +1,72 @@
+//
+//  LocationManager.swift
+//  WeatherApp
+//
+//  Created by Igor Dikan on 22.04.2023.
+//
+
+import CoreLocation
+
+protocol LocationManagable {
+    func fetchLocation() async throws -> CLLocationCoordinate2D
+}
+
+final class LocationManager: NSObject {
+    private let locationManager = CLLocationManager()
+    private let defaultCoordinates = CLLocationCoordinate2D(
+        latitude: Constants.defaultLatitude,
+        longitude: Constants.defaultLongitude
+    )
+    private var checkedThrowingContinuation: CheckedContinuation<CLLocationCoordinate2D, Error>?
+}
+
+extension LocationManager: LocationManagable {
+    func fetchLocation() async throws -> CLLocationCoordinate2D {
+        return try await withCheckedThrowingContinuation{ [weak self] (continuation: CheckedContinuation<CLLocationCoordinate2D, Error>) in
+            self?.locationManager.delegate = self
+            self?.checkedThrowingContinuation = continuation
+        }
+    }
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    func locationManager(
+        _ manager: CLLocationManager,
+        didChangeAuthorization status: CLAuthorizationStatus
+    ) {
+        switch status {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .authorizedAlways,
+             .authorizedWhenInUse,
+             .authorized:
+            locationManager.startUpdatingLocation()
+        case .restricted,
+             .denied:
+            checkedThrowingContinuation?.resume(returning: defaultCoordinates)
+        @unknown default:
+            checkedThrowingContinuation?.resume(returning: defaultCoordinates)
+        }
+    }
+    
+    func locationManager(
+        _ manager: CLLocationManager,
+        didUpdateLocations locations: [CLLocation]
+    ) {
+        if let location = locations.first {
+            locationManager.stopUpdatingLocation()
+            checkedThrowingContinuation?.resume(returning: location.coordinate)
+            checkedThrowingContinuation = nil
+        } else {
+            checkedThrowingContinuation?.resume(returning: defaultCoordinates)
+        }
+    }
+
+    func locationManager(
+        _ manager: CLLocationManager,
+        didFailWithError error: Error
+    ) {
+        checkedThrowingContinuation?.resume(throwing: error)
+        checkedThrowingContinuation = nil
+    }
+}
--- WeatherApp/Scenes/MainPresenter.swift
+++ WeatherApp/Scenes/MainPresenter.swift
@@ -13,9 +13,14 @@ final class MainPresenter {
     weak var view: MainDisplayLogic?
     
     private let apiManager: APIManagable
+    private let locationManager: LocationManagable
     
-    init(apiManager: APIManagable) {
+    init(
+        apiManager: APIManagable,
+        locationManager: LocationManagable
+    ) {
         self.apiManager = apiManager
+        self.locationManager = locationManager
     }
 }
 
@@ -23,9 +28,12 @@ extension MainPresenter: MainPresentationLogic {
     func viewDidLoad() {
         Task {
             do {
-                let request = ForecastRequest(lat: 45.0448400, lon: 38.9760300)
+                let location = try await locationManager.fetchLocation()
+                let request = ForecastRequest(
+                    lat: location.latitude,
+                    lon: location.longitude
+                )
                 let response = try await apiManager.fetchForecast(request: request)
-                
                 await MainActor.run {}
             } catch {
                 await MainActor.run {}
