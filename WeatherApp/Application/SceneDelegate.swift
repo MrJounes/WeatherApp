0aa298ff49b818a0256d70c13b4b95d0fa05a2a9
Igor Dikan <idikan@yandex-team.ru>
2023-04-22 17:31:51 +0300

    Первоначальная настройка проекта

---
+++ WeatherApp/Application/SceneDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  SceneDelegate.swift
+//  WeatherApp
+//
+//  Created by Igor Dikan on 22.04.2023.
+//
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        let rootViewController = ViewController()
+        
+        window.rootViewController = rootViewController
+        window.makeKeyAndVisible()
+        
+        self.window = window
+    }
+}
