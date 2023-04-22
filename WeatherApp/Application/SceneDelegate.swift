56787d2c79100b2d263fcb6f9caad456a7d34fc1
Igor Dikan <idikan@yandex-team.ru>
2023-04-22 17:47:36 +0300

    Добавлен Main модуль и сборщик сцен

--- WeatherApp/Application/SceneDelegate.swift
+++ WeatherApp/Application/SceneDelegate.swift
@@ -18,7 +18,8 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     ) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let rootViewController = ViewController()
+        let sceneBuildManager = SceneBuildManager()
+        let rootViewController = sceneBuildManager.buildMainScene()
         
         window.rootViewController = rootViewController
         window.makeKeyAndVisible()
